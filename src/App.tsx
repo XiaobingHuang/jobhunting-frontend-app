108433b9d2033470ffbb595a84583e48e3e15c51
xhuang35 <xhuang35@buffalo.edu>
2024-01-29 10:07:49 -0500

    install dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,80 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.css";
 
 const client = axios.create({
-  baseURL: 'https://job-hunting-app-cb9818296d91.herokuapp.com/',
+  baseURL: "http://job-hunting-app-cb9818296d91.herokuapp.com",
   timeout: 1000,
-  headers: {
-    'Accept': 'application/vnd.GitHub.v3+json',
-    //'Authorization': 'token <your-token-here> -- https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token'
-  }
 });
-
+interface Idata {
+  profile: string;
+  desc: string;
+  exp: string;
+  techs: string[];
+}
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [data, setData] = useState();
+  const [data, setData] = useState<Idata[]>([]);
 
-  const getAllData= async()=>{
+  const getAllData = async () => {
     setLoading(true);
-    try{
-      const response = await axios.get('http://localhost:8080/posts', {timeout: 1500});
-      console.log(response);
-      // setData(response)
-    }catch (error) {
+    try {
+      // const response = await axios.get('http://localhost:8080/posts', {timeout: 1500});
+      // const response = await axios.get('http://job-hunting-app-cb9818296d91.herokuapp.com/posts', {timeout: 1500});
+      const response = await client.get("/posts");
+      // console.log(response);
+      setData(response.data);
+    } catch (error) {
       console.error(error);
     }
     setLoading(false);
+  };
+
+  useEffect(() => {
+    getAllData();
+  }, []);
+  if (!data) {
+    return <div>Loading...</div>;
   }
 
-  useEffect(()=>{
-    getAllData();
-  },[])
   return (
     <div>
-    {loading && <div>Loading</div>}
-    {!loading && (
-      <div>
-        <h2>Doing stuff with data</h2>
-        {data}
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Job Seeker Posts
+          </a>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+      
+      <div className="container text-center">
+        <div className="row">
+          <div className="col">1 of 2</div>
+          <div className="col">2 of 2</div>
+        </div>
+        <div className="row">
+          <div className="col">1 of 3</div>
+          <div className="col">2 of 3</div>
+          <div className="col">3 of 3</div>
+        </div>
       </div>
-    )}
-    <div>AAAA</div>
+      {/* <ul>
+        {data.map((item,key)=> (
+          <li key={key}>{item.profile}</li>
+        ))}
+      </ul> */}
     </div>
   );
 }
